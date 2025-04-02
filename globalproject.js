56a52257a49257e05c2fa49435810afda382fa59
Goldenkamangu <goldenkamangu20@gmail.com>
2025-04-02 09:46:47 +0200

    Update globalproject.js

--- globalproject.js
+++ globalproject.js
@@ -1,1 +1,6 @@
-
+// This script auto-closes the hamburger menu when any navigation link is clicked.
+document.querySelectorAll('nav ul li a').forEach(link => {
+  link.addEventListener('click', () => {
+    document.getElementById('menu-toggle').checked = false;
+  });
+});
